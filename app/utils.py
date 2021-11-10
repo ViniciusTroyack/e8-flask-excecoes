375f407137ab635c71ff7ed24aa42547c95c52d9
Vinicius <troyackvinicius@gmail.com>
2021-11-10 01:23:59 -0300

    Fix: criando diretorio para o database

--- app/utils.py
+++ app/utils.py
@@ -9,10 +9,15 @@ def is_empty():
         with open("./app/database/database.json", "w") as json_file:
             json.dump(new_database, json_file, indent=4)
 
+
 def database_exist():
+    if not os.path.isdir(DATABASE_DIRECTORY):
+        os.mkdir(DATABASE_DIRECTORY)
+
     if not 'database.json' in os.listdir(DATABASE_DIRECTORY):
         with open(f"{DATABASE_DIRECTORY}/database.json", "a"):
             ...
+
 
 def verify_email_exist(email):
     with open("./app/database/database.json", "r") as json_file:
@@ -21,3 +26,5 @@ def verify_email_exist(email):
             email_list = [user['email'] for user in user_list['data']]
             
     return email in email_list 
+
+print('ok')
